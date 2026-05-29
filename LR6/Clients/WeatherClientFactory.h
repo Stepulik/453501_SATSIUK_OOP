f5c76c296b7838c3b47d8c3fdb30b6e97edef889
Stepulik <stepan.satsuk@yandex.ru>
2026-05-29 16:09:37 +0000

    GREEN: Implement WeatherClientFactory, update CurrentWeatherController

---
+++ LR6/Clients/WeatherClientFactory.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <functional>
+#include "WeatherDataClient.h"
+
+namespace Forecast::Clients {
+    class WeatherClientFactory {
+    public:
+        using Creator = std::function<std::shared_ptr<IWeatherDataClient>()>;
+
+        void registerClient(const std::string& name, Creator creator) {
+            creators[name] = creator;
+        }
+
+        std::shared_ptr<IWeatherDataClient> create(const std::string& name) const {
+            auto it = creators.find(name);
+            if (it != creators.end())
+                return it->second();
+            return nullptr;
+        }
+
+    private:
+        std::unordered_map<std::string, Creator> creators;
+    };
+}
--- LR6/Controllers/CurrentWeatherController.h
+++ LR6/Controllers/CurrentWeatherController.h
@@ -2,27 +2,37 @@
 #include <memory>
 #include <future>
 #include <string>
+#include <stdexcept>
 #include "../Clients/WeatherDataClient.h"
+#include "../Clients/WeatherClientFactory.h"
 #include "../Models/Weather/CurrentWeather.h"
 
 namespace Forecast::Controllers {
     class CurrentWeatherController {
     private:
-        std::shared_ptr<Clients::IWeatherDataClient> client;
+        std::shared_ptr<Clients::WeatherClientFactory> factory;
     public:
-        explicit CurrentWeatherController(std::shared_ptr<Clients::IWeatherDataClient> client)
-            : client(std::move(client)) {}
+        // Новый конструктор (принимает фабрику)
+        explicit CurrentWeatherController(std::shared_ptr<Clients::WeatherClientFactory> factory_)
+            : factory(std::move(factory_)) {}
 
-        // Новый метод с параметром provider
+        // Старый конструктор (принимает одного клиента) – для обратной совместимости
+        explicit CurrentWeatherController(std::shared_ptr<Clients::IWeatherDataClient> client) {
+            factory = std::make_shared<Clients::WeatherClientFactory>();
+            factory->registerClient("openweather", [client]() { return client; });
+        }
+
         std::future<Models::Weather::CurrentWeather> GetCurrentWeather(double latitude, double longitude, const std::string& provider) {
-            // provider игнорируется
-            return std::async(std::launch::async, [this, latitude, longitude]() {
+            auto client = factory->create(provider);
+            if (!client) {
+                throw std::runtime_error("Unknown provider: " + provider);
+            }
+            return std::async(std::launch::async, [client, latitude, longitude]() {
                 double temperature = client->LocationCurrentTemperature(latitude, longitude).get();
                 return Models::Weather::CurrentWeather{ temperature };
             });
         }
 
-        // Старый метод для совместимости
         std::future<Models::Weather::CurrentWeather> GetCurrentWeather(double latitude, double longitude) {
             return GetCurrentWeather(latitude, longitude, "openweather");
         }
