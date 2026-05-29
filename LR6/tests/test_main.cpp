36a71d7d0719996f74451300f557435edb816df2
Stepulik <stepan.satsuk@yandex.ru>
2026-05-29 14:37:56 +0000

    Refactor tests: 7 passing unit tests for CurrentWeatherController with edge cases

--- LR6/tests/test_main.cpp
+++ LR6/tests/test_main.cpp
@@ -2,8 +2,10 @@
 #include <future>
 #include "../Clients/WeatherDataClient.h"
 #include "../Controllers/CurrentWeatherController.h"
+#include "../Api/WeatherApi.h"
 #include "../Utils/ApiCallException.h"
 
+//  Мок-клиент для тестов 
 class MockWeatherClient : public Forecast::Clients::IWeatherDataClient {
 public:
     double returnValue = 25.0;
@@ -19,40 +21,76 @@ public:
     }
 };
 
-TEST(CurrentWeatherControllerTest, ReturnsCorrectTemperature) {
-    auto mock = std::make_shared<MockWeatherClient>();
-    mock->returnValue = 22.5;
-    Forecast::Controllers::CurrentWeatherController ctrl(mock);
-    auto result = ctrl.GetCurrentWeather(52.0, 21.0).get();
+//  Фикстура для тестов CurrentWeatherController 
+class CurrentWeatherControllerTest : public ::testing::Test {
+protected:
+    std::shared_ptr<MockWeatherClient> mockOpenWeather;
+    std::shared_ptr<Forecast::Controllers::CurrentWeatherController> controller;
+
+    void SetUp() override {
+        mockOpenWeather = std::make_shared<MockWeatherClient>();
+        controller = std::make_shared<Forecast::Controllers::CurrentWeatherController>(mockOpenWeather);
+    }
+};
+
+//  СТАРЫЕ ТЕСТЫ 
+
+// Проверяет, что контроллер возвращает температуру, переданную от клиента (без изменений)
+TEST_F(CurrentWeatherControllerTest, ReturnsCorrectTemperature) {
+    mockOpenWeather->returnValue = 22.5;
+    auto result = controller->GetCurrentWeather(52.0, 21.0).get();
     EXPECT_DOUBLE_EQ(result.temperature, 22.5);
 }
 
-TEST(CurrentWeatherControllerTest, PropagatesApiException) {
-    auto mock = std::make_shared<MockWeatherClient>();
-    mock->shouldThrow = true;
-    Forecast::Controllers::CurrentWeatherController ctrl(mock);
+// Проверяет, что если клиент выбрасывает ApiCallException, контроллер пробрасывает его дальше
+TEST_F(CurrentWeatherControllerTest, PropagatesApiException) {
+    mockOpenWeather->shouldThrow = true;
     EXPECT_THROW(
-        ctrl.GetCurrentWeather(0.0, 0.0).get(),
+        controller->GetCurrentWeather(0.0, 0.0).get(),
         Forecast::Utils::ApiCallException
     );
 }
 
-TEST(CurrentWeatherControllerTest, NegativeCoordinatesWork) {
-    auto mock = std::make_shared<MockWeatherClient>();
-    mock->returnValue = -10.0;
-    Forecast::Controllers::CurrentWeatherController ctrl(mock);
-    auto result = ctrl.GetCurrentWeather(-90.0, -180.0).get();
+// Проверяет, что контроллер работает с отрицательными координатами
+TEST_F(CurrentWeatherControllerTest, NegativeCoordinatesWork) {
+    mockOpenWeather->returnValue = -10.0;
+    auto result = controller->GetCurrentWeather(-90.0, -180.0).get();
     EXPECT_DOUBLE_EQ(result.temperature, -10.0);
 }
 
-TEST(CurrentWeatherControllerTest, ZeroTemperature) {
-    auto mock = std::make_shared<MockWeatherClient>();
-    mock->returnValue = 0.0;
-    Forecast::Controllers::CurrentWeatherController ctrl(mock);
-    auto result = ctrl.GetCurrentWeather(0.0, 0.0).get();
+// Проверяет, что контроллер корректно обрабатывает нулевую температуру
+TEST_F(CurrentWeatherControllerTest, ZeroTemperature) {
+    mockOpenWeather->returnValue = 0.0;
+    auto result = controller->GetCurrentWeather(0.0, 0.0).get();
     EXPECT_DOUBLE_EQ(result.temperature, 0.0);
 }
 
+//  НОВЫЕ ТЕСТЫ (дополнение для пограничных случаев) 
+
+// Проверяет, что метод GetCurrentWeather с явным указанием провайдера "openweather" работает
+TEST_F(CurrentWeatherControllerTest, ExplicitProviderOpenWeather) {
+    mockOpenWeather->returnValue = 25.0;
+    auto result = controller->GetCurrentWeather(52.0, 21.0, "openweather").get();
+    EXPECT_DOUBLE_EQ(result.temperature, 25.0);
+}
+
+// Проверяет, что пока не реализована логика выбора провайдера, любой строковый provider принимается
+// (возвращается температура от единственного клиента). В будущем это изменится.
+TEST_F(CurrentWeatherControllerTest, AnyProviderIsAccepted) {
+    mockOpenWeather->returnValue = 18.0;
+    auto result = controller->GetCurrentWeather(52.0, 21.0, "unknown_provider").get();
+    EXPECT_DOUBLE_EQ(result.temperature, 18.0);
+}
+
+// Проверяет, что при передаче экстремальных значений координат (очень большие числа) контроллер не падает
+TEST_F(CurrentWeatherControllerTest, ExtremeCoordinates) {
+    mockOpenWeather->returnValue = -99.9;
+    auto result = controller->GetCurrentWeather(1e6, -1e6, "openweather").get();
+    EXPECT_DOUBLE_EQ(result.temperature, -99.9);
+}
+
+
+// MAIN 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
